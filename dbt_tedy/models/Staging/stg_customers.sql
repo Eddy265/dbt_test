a8048ee87aa9a926eecdbefa0b5b9f31cb0be57d
Edwin Uzoefuna <euzoefuna@rocketmail.com>
2024-04-29 18:40:00 +0200

    started

---
+++ dbt_tedy/models/Staging/stg_customers.sql
@@ -0,0 +1,19 @@
+
+
+WITH source AS (
+
+    SELECT * FROM {{ source('Instacart', 'customers') }}
+),
+name_merge AS (
+    SELECT customer_id,
+        CONCAT(first_name, ' ', last_name) AS customer_name,
+        email,
+        address,
+        phone_number,
+        country,
+        age,
+        gender
+FROM source
+)
+
+SELECT * FROM name_merge
